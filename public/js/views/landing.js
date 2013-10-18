6786a241fbce6775e19c690141104403e984f16e
Natarajan Shanker <rajushank84@yahoo.com>
2013-10-17 21:39:47 -0700

    Loading map markers from a JS collection

--- public/js/views/landing.js
+++ public/js/views/landing.js
@@ -58,11 +58,42 @@ define([
 
 			initializeMap: function() {
 			    var mapOptions = {
-			          zoom: 14,
+			          zoom: 12,
 			          //center: new google.maps.LatLng(-34.397, 150.644),
 			          mapTypeId: google.maps.MapTypeId.ROADMAP
 			        },
-			        map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
+			        map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions),
+			        ads,
+			        infoBox = [],
+			        i,
+			        that = this;
+
+			    ads = [
+					{
+						pos: {lb: 37.355285, mb: -121.906013},
+						content: "Honda Civic wheels for sale"
+					},
+					{
+						pos: {lb: 37.344641,
+						mb: -121.932821},
+						content: "Homemade cookies"	
+					},
+					{
+						pos: {lb: 37.396823,
+						mb: -121.958728},
+						content: "Moving sale - everything must go. Furniture, household items"
+					},
+					{
+						pos: {lb: 37.427911,
+						mb: -121.880107},
+						content: "Baby stroller suitable for 1 to 3 year-olds"
+					},
+					{
+						pos: {lb: 37.379024,
+						mb: -121.981544},
+						content: "Want to buy - used vacuum cleaner less than $75"
+					}
+				];
 
 			    // Try HTML5 geolocation
 			    if (navigator.geolocation) {
@@ -70,26 +101,32 @@ define([
 			            var pos = new google.maps.LatLng(position.coords.latitude,
 			                position.coords.longitude);
 
+			            map.setCenter(pos);
+						for(i=0; i<ads.length; i++) {
+							that.createBox(ads[i].pos.lb, ads[i].pos.mb, ads[i].content);
 
-
-			            var infoBox = new InfoBox({
-			                position: pos,
-			                map: map,
-			                content: "<div class='infoBox'>You are here</div>"
-			            });
-
-			            infoBox.open(map);
+				         }
 
 			            map.setCenter(pos);
-
 			        }, function () {
 			            handleNoGeolocation();
 			        });
 			    } else {
 			        handleNoGeolocation();
 			    }
-
+			    
 			    this.map = map;
+			},
+
+			createBox: function(lat, lon, html) {
+			    var map = this.map,
+			    	newBox = new InfoBox({
+				        position: new google.maps.LatLng(lat, lon),
+				        map: map,
+			            content: html
+			        });
+
+    			newBox.open(map);
 			},
 
 			handleNoGeolocation: function() {
